7449e53f7ab0e113113e3ed97fd885ed667d2fdd
Lincoln Bryant <lincolnb@mit.edu>
2026-06-16 15:35:42 -0400

    add a little state diagram , make dhcp offers work

--- src/ebb_dhcpd.erl
+++ src/ebb_dhcpd.erl
@@ -11,7 +11,9 @@ servers.
 
 %% API
 -export([
-    start_link/0
+    start_link/0,
+    % for testing
+    route_msg/2
 ]).
 
 %% gen_server callbacks
@@ -28,7 +30,7 @@ start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 init([]) ->
-    {ok, Socket} = gen_udp:open(67, [{active, once}, binary]),
+    {ok, Socket} = gen_udp:open(67, [{active, once}, {broadcast, true}, binary]),
     {ok, #{
         socket => Socket
     }}.
@@ -51,10 +53,10 @@ handle_cast(_Msg, State) ->
 %                            ▼
 %                    ┌────────────────┐
 %                    │   REQUESTING   │──── NAK ───────► (restart)
-%                    └───────┬────────┘               
-%                            │ ACK                    
-%                            ▼                        
-%                     ┌──────────────┐    
+%                    └───────┬────────┘
+%                            │ ACK
+%                            ▼
+%                     ┌──────────────┐
 %                     │    BOUND     │
 %                     └──────┬───────┘
 %                            │ T1
@@ -72,12 +74,12 @@ handle_cast(_Msg, State) ->
 %                          INIT
 
 handle_info({udp, Socket, _Addr, _Port, Packet}, State) ->
-	% Rearm the socket for another message
+    % Rearm the socket for another message
     inet:setopts(Socket, [{active, once}]),
-	Msg = ebb_dhcp_packet:decode(Packet),
-	% Consume the packet and send a reply as a side effect, as necessary
-	route_msg(Msg, Socket),
-	{noreply, State};
+    Msg = ebb_dhcp_packet:decode(Packet),
+    % Consume the packet and send a reply as a side effect, as necessary
+    route_msg(Msg, Socket),
+    {noreply, State};
 handle_info(_Msg, State) ->
     {noreply, State}.
 
@@ -87,68 +89,78 @@ terminate(_Reason, _State) ->
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
-route_msg(Msg = #dhcp_message{ op = bootrequest, options = Options }, Socket) ->
-	MsgType = proplists:get_value(message_type, Options),
-	case MsgType of 
-		dhcpdiscover -> 
-			Offer = ebb_dhcp_table_mem:create_offer(Msg),
-			send_offer(Msg, Offer, Socket);
-		dhcprequest -> 
-			ebb_dhcp_table_mem:accept_offer(Msg, Socket);
-		_ -> 
-			ok
-	end;
-route_msg(_Msg, _) -> 
-	logger:debug("Discarding non-implemented message type").
+route_msg(Msg = #dhcp_message{op = bootrequest, options = Options}, Socket) ->
+    MsgType = proplists:get_value(message_type, Options),
+    case MsgType of
+        dhcpdiscover ->
+            OfferLease =
+                case ebb_dhcp_pool_mem:get_offer(Msg) of
+                    false ->
+                        ebb_dhcp_pool_mem:create_offer(Msg);
+                    Existing ->
+                        Existing
+                end,
+            send_offer(Msg, OfferLease, Socket);
+        dhcprequest ->
+            ok;
+        % TODO:
+        %ebb_dhcp_pool_mem:accept_offer(Msg, Socket);
+        _ ->
+            % TODO: Handle other message types
+            ok
+    end;
+route_msg(_Msg, _) ->
+    logger:debug("Discarding non-implemented message type").
 
 send_offer(DiscoverMsg, Offer, Socket) ->
-	% Retrieve fields from the original message
-	#dhcp_message{
-	  xid = Xid,
-	  chaddr = ChAddr,
-	  giaddr = GiAddr,
-	  htype = HType,
-	  hlen = HLen
-	} = DiscoverMsg,
-	% Pull the client IP out of the offer
-	#dhcp_lease{
-	   ip = ClientIP,
-	   duration = Duration
-	} = Offer,
-	% Grab the server IP from the socket
-	{ok, {ServerIP, _Port}} = inet:sockname(Socket),
-	% Create the options list
-	Options = [
-			   {1, 12},
-			   {51, Duration},
-			   {53, dhcpoffer},
-			   {54, ServerIP}
-			  ],
-	OfferMsg = #dhcp_message{
-				  op = bootreply,
-				  htype = HType,
-				  hlen = HLen,
-				  hops = 0,
-				  xid = Xid,
-				  secs = 0,
-				  flags = [broadcast],
+    % Retrieve fields from the original message
+    #dhcp_message{
+        xid = Xid,
+        chaddr = ChAddr,
+        giaddr = GiAddr,
+        htype = HType,
+        hlen = HLen
+    } = DiscoverMsg,
+    % Pull the client IP out of the offer
+    #dhcp_lease{
+        ip = ClientIP,
+        subnet_mask = SubnetMask,
+        duration = Duration
+    } = Offer,
+    % Grab the server IP from the socket
+    {ok, {ServerIP, _Port}} = inet:sockname(Socket),
+    % Create the options list
+    Options = [
+        {subnet_mask, SubnetMask},
+        {lease_time, Duration},
+        {message_type, dhcpoffer},
+        {server_id, ServerIP}
+    ],
+    OfferMsg = #dhcp_message{
+        op = bootreply,
+        htype = HType,
+        hlen = HLen,
+        hops = 0,
+        xid = Xid,
+        secs = 0,
+        flags = [broadcast],
 
-				  ciaddr = 0,
-				  yiaddr = ClientIP,
-				  siaddr = ServerIP,
-				  giaddr = GiAddr,
-				  chaddr = ChAddr,
+        ciaddr = {0, 0, 0, 0},
+        yiaddr = ClientIP,
+        siaddr = ServerIP,
+        giaddr = GiAddr,
+        chaddr = ChAddr,
 
-				  sname = <<>>,
-				  file = <<>>,
+        sname = <<>>,
+        file = <<>>,
 
-				  options = Options
-				},
-	logger:notice("Created message: ~p", [OfferMsg]),
-	OfferPacket = ebb_dhcp_packet:encode(OfferMsg),
-	logger:notice("Encoded message: ~p", [OfferPacket]),
-	% TODO : Remove broadcast flast, unicast directly to the MAC
-	gen_udp:send(Socket, {255,255,255,255}, 68, OfferPacket).
+        options = Options
+    },
+    logger:notice("Created message: ~p", [OfferMsg]),
+    OfferPacket = ebb_dhcp_packet:encode(OfferMsg),
+    logger:notice("Encoded message: ~p", [OfferPacket]),
+    % TODO : Remove broadcast flast, unicast directly to the MAC
+    gen_udp:send(Socket, {255, 255, 255, 255}, 68, OfferPacket).
 
 %maybe_lease(Msg, LeaseTable) ->
 %    Options = Msg#dhcp_message.options,
@@ -156,7 +168,7 @@ send_offer(DiscoverMsg, Offer, Socket) ->
 %    MsgType = proplists:get_value(message_type, Options),
 %    case MsgType of
 %        dhcpdiscover ->
-%            Lease = ebb_dhcp_table_mem:get_or_create(MacAddr),
+%            Lease = ebb_dhcp_pool_mem:get_or_create(MacAddr),
 %            {Lease, LeaseTable};
 %        _ ->
 %            logger:debug("Not implemented"),
